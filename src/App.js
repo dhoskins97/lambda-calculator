bada524425de7686c3b21f42080e9462c4cb916c
Dakota Hoskins <dakotaahoskinss@gmail.com>
2019-07-17 18:47:40 -0400

    mvp reached, forgot to push

--- src/App.js
+++ src/App.js
@@ -21,8 +21,11 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
-        <Numbers />
+        <div className="display">0</div>
         <Specials />
+        
+        <Numbers />
+        
         <Operators />
       </div>
     </div>
